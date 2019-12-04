41bf4b7bb040a90ba7853a1fb2cce0f89aca50a4
Ryan Felter <42056419+ryansCorner@users.noreply.github.com>
2019-12-04 13:31:54 -0800

    packages updated

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 let express = require('express')
 let request = require('request')
 let querystring = require('querystring')
+const dotenv = require("dotenv");
+
+// let db = require('db')
+dotenv.config();
 
 let app = express()
+let client_id = process.env.DB_SPOTIFY_CLIENT_ID
+let client_secret = process.env.DB_SPOTIFY_CLIENT_SECRET; // Your secret
 
-let redirect_uri = 
-  process.env.REDIRECT_URI || 
-  'http://localhost:8888/callback'
+let redirect_uri = process.env.DB_REDIRECT_URI || 'http://localhost:8888/callback'
 
-app.get('/login', function(req, res) {
+
+app.get('/loginSpotify', function (req, res) {
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
-      client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      client_id: client_id,
+      scope: 'user-read-private user-read-email playlist-read-collaborative playlist-modify-private playlist-modify-public playlist-read-private user-modify-playback-state user-read-currently-playing user-read-playback-state user-library-modify user-library-read user-follow-modify user-follow-read user-read-recently-played user-top-read streaming app-remote-control',
       redirect_uri
     }))
 })
 
-app.get('/callback', function(req, res) {
+app.get('/callback', function (req, res) {
   let code = req.query.code || null
   let authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -29,14 +34,14 @@ app.get('/callback', function(req, res) {
     },
     headers: {
       'Authorization': 'Basic ' + (new Buffer(
-        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+        client_id + ':' + client_secret
       ).toString('base64'))
     },
     json: true
   }
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     var access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    let uri = process.env.DB_FRONTEND_URI || 'http://localhost:3000'
     res.redirect(uri + '?access_token=' + access_token)
   })
 })
